d08d1584ffd4581c3bc89d32ff19d05ef859480b
Mo Prosper <mothepro@live.com>
2023-04-04 13:29:35 -0500

    log

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import 'aframe'
+import * as s from 'aframe'
 
 setTimeout(() => alert('This is from mo!'), 5_000)
+console.warn({ s })
 document.querySelector('a-entity').addEventListener('model-error', console.error)
 document.querySelector('a-entity').addEventListener('model-loaded', console.warn)
