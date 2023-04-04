14fc6b32f0f2e36c454e888d05fcdf992fb0f080
Mo Prosper <mothepro@live.com>
2023-04-03 22:47:06 -0500

    include aframe

---
+++ chat-gpt.ts
@@ -0,0 +1,16 @@
+// Prompt
+// create a model of a blue sphere in typescript using the aframe vr library
+import * as AFRAME from 'aframe'
+
+const scene = document.querySelector('a-scene')
+
+// Create a blue sphere entity
+const sphere = document.createElement('a-entity')
+sphere.setAttribute('geometry', {
+  primitive: 'sphere',
+  radius: 1,
+})
+sphere.setAttribute('material', {
+  color: 'blue',
+})
+scene.appendChild(sphere)
--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
 // Remember to append ".js" for relative imports
+setTimeout(() => alert('This is from mo!'), 15_000)
